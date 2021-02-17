9bfca9ab00834fb4759d2660a2699bd96f30e568
timothy lampung <timothylampung@gmail.com>
2021-02-17 16:27:35 +0800

    major updates

--- transporter/xarm_scripts/rackc.py
+++ transporter/xarm_scripts/rackc.py
@@ -15,16 +15,19 @@ class RackC(BaseMovement):
                 self.x_arm.move_arc_line(*[-342.4, -6.5, -76.9, -179.9, 0.0, 177.3], radius=50, wait=True)
                 self.vacuum_in('ayam', 5000)
                 self.x_arm.move_arc_line(*[-342.5, -6.5, 34.7, -179.9, 0.0, 177.3], radius=50, wait=True)
+                self.x_arm.move_arc_line(*[-156.4, 354.6, 19.2, 180, 0.0, 118], radius=50, wait=True)
             elif self.ingredient == 'LIQUID':
                 self.x_arm.move_join(angle=[155.3, -17.2, -41.2, 0.1, 58.4, -3.9], radius=50, wait=False)
                 self.x_arm.move_arc_line(*[-341.2, 156.1, -73.4, -179.9, 0.0, 159.2], radius=50, wait=True)
                 self.suck(9000)
                 self.x_arm.move_arc_line(*[-341.2, 156.1, 44.1, -179.9, 0.0, 159.2], radius=50, wait=True)
+                self.x_arm.move_arc_line(*[-156.4, 354.6, 19.2, 180, 0.0, 118], radius=50, wait=True)
             else:
                 self.x_arm.move_join(angle=[166.1, -25.8, -32.2, 0.1, 58.0, -15.1], radius=50, wait=False)
                 self.x_arm.move_arc_line(*[-322.2, 79.4, -66.1, -179.9, 0.0, -178.9], radius=50, wait=True)
                 self.vacuum_powder("ayam", EIG.MAX_GRANULAR)
                 self.x_arm.move_arc_line(*[-322.2, 79.4, 33.7, -179.9, 0.0, -178.9], radius=50, wait=True)
+                self.x_arm.move_arc_line(*[-156.4, 354.6, 19.2, 180, 0.0, 118], radius=50, wait=True)
 
             if self.requester_callback is not None:
                 self.requester_callback()
